0a6e31e5648faba5f33ba4941eb7aba5c1fe6c1b
danang95 <nang.dev8@gmail.com>
2025-11-22 08:56:21 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,21 @@
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    // kalau belum login arahkan ke login
+    if (!auth()->check()) {
+        return redirect('/login');
+    }
+
+    // kalau sudah login arahkan sesuai level
+    if (auth()->user()->user_level == 'Admin') {
+        return redirect('/admin/dashboard');
+    } else {
+        return redirect('/user/dashboard');
+    }
+});
+
+
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/user.php';
